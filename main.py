f6299e4854ea175705915132761caad90793c678
hong <daggax8235@gmail.com>
2025-02-10 09:36:35 +0900

    대략적인 구도 작성

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-from src.locker import Locker, LockerException
-from src.logger_config import setup_logger
+import asyncio
+import logging
+import json
+
+from src.locker import Locker
+from supa_db.supa_db import SupaDB
+from supa_realtime.config import DATABASE_URL, JWT
+from supa_realtime.realtime_service import RealtimeService
 
 
-def main():
-    # 로거 설정
-    logger = setup_logger()
-    locker = None
-    port = 'COM6'
+class LaundryHandler:
+    def __init__(self):
+        self.supa_api = SupaDB()
+        self.service = RealtimeService(DATABASE_URL, JWT, self.handle_change)
+        self.locker = Locker("/dev/ttyxx")
 
-    try:
-        locker = Locker(port)
+    async def handle_change(self, payload):
+        logging.info(f"Database change detected: {payload}")
+        record = payload['data']['record']
+        '''
+        순서가 대충
+        payload에 오는 row에 stoarge id가 포함되서오는데 일단 누가 요청했는지 
+        
+        그럼 payload 에 바로 누가 신청했는지 req id 가 있으니
+        해당 id로 profile table에 role 조회후 해당 role에 따라 분기가 나누어짐
+        
+        role user:
+        유저는 결제가 완료가 되어야지만 자신의 사물함을 열수있음.
+        그러면 해당 storage의 laundry id로 조회해서
+        해당 세탁 건이 결제가 되었다. 그러면 open으로 바꾸고 열림 로직 실행시키고,
+        안되었다 하면 req를 reject으로 바꾸고 안열리게 하면됨.
+        
+        role deliver or manager:
+        그냥 열어줌
+        '''
 
-        # 상태 확인
-        status = locker.is_locked(1)
-        logger.info(f"1번 사물함 상태: {'잠김' if status else '열림'}")
+    async def start(self):
+        await self.service.start_listening()
 
-        # 열기 시도
-        if status:
-            success = locker.open(1)
-            logger.info(f"1번 사물함 열기: {'성공' if success else '실패'}")
 
-    except LockerException as e:
-        logger.error(f"사물함 오류: {str(e)}")
-        raise
-    except Exception as e:
-        logger.error(f"예상치 못한 오류: {str(e)}")
-        raise
-    finally:
-        if locker:
-            locker.close()
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    handler = LaundryHandler()
+    await handler.start()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
